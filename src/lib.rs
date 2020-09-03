c60a0602b25156570bf017f9ede23746c3cfb874
Nathan Marley <nathan.marley@gmail.com>
2020-09-03 12:34:15 -0300

    Implement serach

--- src/lib.rs
+++ src/lib.rs
@@ -26,7 +26,14 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    vec![]
+    let mut results: Vec<&str> = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            // add a ref to vec
+            results.push(line);
+        }
+    }
+    results
 }
 
 #[cfg(test)]
