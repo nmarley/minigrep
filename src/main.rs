c4fd2afbbf162a18007f9dcbff92197d3271ac0a
Nathan Marley <nathan.marley@gmail.com>
2020-09-03 11:33:34 -0300

    small refactor 1

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,18 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-
-    let query = &args[1];
-    let filename = &args[2];
+    let (query, filename) = parse_config(&args);
 
     println!("Searching for {}", query);
     println!("... in file {}", filename);
 
     let contents = fs::read_to_string(filename).expect("unable to read file");
     println!("with text:\n{}", contents)
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let filename = &args[2];
+
+    (query, filename)
 }
