712929e13e2d0cea82cb3a8b5c2623c0e96af2f9
Nathan Marley <nathan.marley@gmail.com>
2020-09-03 12:10:44 -0300

    better errors w/run and main

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::error::Error;
 use std::fs;
 use std::process;
 
@@ -12,12 +13,18 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("... in file {}", config.filename);
 
-    run(config);
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
 }
 
-fn run(config: Config) {
-    let contents = fs::read_to_string(config.filename).expect("unable to read file");
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+
     println!("With text:\n{}", contents);
+
+    Ok(())
 }
 
 struct Config {
