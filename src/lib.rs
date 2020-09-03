82e6f74c4ca25ba6175bca3d888b4c25c1c6c7d7
Nathan Marley <nathan.marley@gmail.com>
2020-09-03 12:20:33 -0300

    add pub visibility qualifier

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,12 @@
 use std::error::Error;
 use std::fs;
 
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.filename)?;
-
-    println!("With text:\n{}", contents);
-
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    filename: String,
+pub struct Config {
+    pub query: String,
+    pub filename: String,
 }
 impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
             return Err("not enough arguments");
         }
@@ -23,4 +15,12 @@ impl Config {
 
         Ok(Config { query, filename })
     }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+
+    println!("With text:\n{}", contents);
+
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::env;
 use std::process;
 
+use minigrep::Config;
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::new(&args).unwrap_or_else(|err| {
@@ -11,7 +13,7 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("... in file {}", config.filename);
 
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("Application error: {}", e);
         process::exit(1);
     }
